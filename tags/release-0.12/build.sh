070afe64cbf01b10f51e5139962012f979215c39
tarick@bedeir.com <tarick@bedeir.com@d15b875e-a8c7-13e1-a113-f0b195c0232e>
2012-09-18 03:00:22 +0000

    tag for 0.12 release

---
+++ tags/release-0.12/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+VERSION=`head -n 1 ChangeLog | sed -e 's/.*(//' -e 's/).*//'`
+
+cat configure.ac.in | sed -e "s/__VERSION__/$VERSION/g" > configure.ac
+autoreconf --force --install || exit 1
+
+if [ "$(uname)" == "Darwin" ]; then
+  ./configure --enable-tests --enable-darwin
+else
+  ./configure --enable-tests
+fi
