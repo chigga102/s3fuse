a790374accfa22ecd3d68c955cf6733f9e82c01d
tarick@bedeir.com <tarick@bedeir.com@d15b875e-a8c7-13e1-a113-f0b195c0232e>
2012-09-24 21:51:44 +0000

    branch for object refactor off 0.12

---
+++ branches/0.12-object-refactor/clean.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ ! -d src ] || [ ! -d dist ]; then
+  echo Run me from the top-level directory.
+  exit 1
+fi
+
+
+for D in src src/tests dist; do
+  pushd $D >& /dev/null
+    make clean distclean >& /dev/null
+    rm -rf .deps >& /dev/null
+    rm -f Makefile Makefile.in
+    rm -f s3fuse.conf
+  popd >& /dev/null
+done
+
+find . -maxdepth 1 -type f -name \*.m4 ! -name boost.m4 | xargs rm -f
+rm -rf autom4te.cache
+rm -f config.*
+rm -f configure
+rm -f configure.ac
+rm -f depcomp
+rm -f install-sh
+rm -f Makefile Makefile.in
+rm -f missing
+rm -f *.tar.gz
+rm -f libtool
+rm -f ltmain.sh
