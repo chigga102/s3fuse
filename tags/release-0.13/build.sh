15d186f528e0188a99d5b8190ee52bc6e2a460af
tarick@bedeir.com <tarick@bedeir.com@d15b875e-a8c7-13e1-a113-f0b195c0232e>
2013-02-10 16:52:26 +0000

    undo 0.13 release

--- tags/release-0.13/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-autoreconf --force --install || exit 1
-
-if [ "$(uname)" == "Darwin" ]; then
-  ./configure --enable-tests --enable-darwin $*
-else
-  ./configure --enable-tests $*
-fi
