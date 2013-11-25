d343dec89dd0860dc0a7f38d5ef13cff8106c050
tarick <tarick@bedeir.com>
2013-11-25 00:44:58 +0000

    tag packages for 0.15-1 release

---
+++ scripts/build-ubuntu.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+_DIST=$1
+_SERIES=$2
+
+PKG_DIR=$(pwd)
+
+function die()
+{
+  echo $*
+  exit 1
+}
+
+[[ ! -z "$_SERIES" ]] || die "Usage: $0 <dist-tarball> <series>"
+[[ -f "$_DIST" ]] || die "Can't find dist package [$_DIST]."
+[[ -d ubuntu ]] || die "Run me from the top-level packaging directory."
+[[ "$_DIST" == "${_DIST%%.tar.gz}.tar.gz" ]] || die "Tarball name needs to end in .tar.gz."
+
+DIST_NAME="$(basename $_DIST)"
+DIST_NAME="${DIST_NAME%%-*}"
+CL_FILE="changelog.$DIST_NAME.$_SERIES"
+
+[[ -f "ubuntu/$CL_FILE" ]] || die "Expected to find changelog file [ubuntu/$CL_FILE]."
+
+pushd $(dirname $_DIST) >/dev/null || die "Can't enter dist dir."
+_DIST=$(pwd)/$(basename $_DIST)
+popd >/dev/null
+
+mkdir -p output || die "Can't create output dir."
+
+rm -rf build/ub-build
+mkdir -p build/ub-build || die "Can't create build dir."
+cd build/ub-build || die "Can't enter build dir."
+
+TAR_NAME=$(basename $_DIST)
+TAR_NAME=${TAR_NAME%%.tar.gz}
+cp $_DIST ${TAR_NAME/-/_}.orig.tar.gz || die "Failed to copy tarball."
+
+tar xfz $_DIST || die "Failed to unpack tarball."
+cd * || die "Failed to enter source dir."
+
+cp -r $PKG_DIR/debian . || die "Can't copy debian files."
+cd debian || die "Can't enter debian"
+
+find . -type d -name .svn | xargs rm -rf
+
+rm changelog.* || die "Can't remove existing changelog"
+cp $PKG_DIR/ubuntu/control.in . || die "Can't copy Ubuntu control file."
+
+$PKG_DIR/scripts/merge-changelogs.py \
+  $PKG_DIR/debian/changelog.$DIST_NAME \
+  $PKG_DIR/ubuntu/changelog.$DIST_NAME.$_SERIES \
+  > changelog \
+  || die "Can't merge changelogs"
+
+CL_FULL_VERSION="$(head -n 1 changelog | sed -e 's/.* (//' -e 's/).*//')"
+
+for F in *.in; do
+  sed \
+    -e "s/__PACKAGE_NAME__/$DIST_NAME/g" \
+    -e "s/__PACKAGE_VERSION__/$CL_FULL_VERSION/g" \
+    < $F \
+    > ${F%%.in}
+done
+
+debuild || die "debuild failed."
+debuild -S -sa || die "debuild (source) failed."
+
+cd $PKG_DIR
+
+mv build/ub-build/*.tar.gz output/
+mv build/ub-build/*.dsc output/
+mv build/ub-build/*.build output/
+mv build/ub-build/*.changes output/
+mv build/ub-build/*.deb output/ || die "Can't move .deb files."
+
+rm -rf build/ub-build
+rmdir build 2>/dev/null
---
+++ scripts/generate-gcsfs.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+_TRUNK_DIR=$1
+shift
+
+ORIG_NAME=s3fuse
+PKG_NAME=gcsfs
+PKG_DIR=$(pwd)
+
+function die()
+{
+  echo $*
+  exit 1
+}
+
+[[ -d scripts ]] || die "Run me from the top-level packaging directory."
+[[ ! -z "$_TRUNK_DIR" ]] || die "Usage: $0 <trunk-dir>"
+[[ -f "$_TRUNK_DIR/ChangeLog" ]] || die "Can't find ChangeLog in [$_TRUNK_DIR]."
+
+pushd "$_TRUNK_DIR" >/dev/null || die "Can't change into trunk dir."
+_TRUNK_DIR="$(pwd)"
+popd >/dev/null
+
+echo "using trunk dir [$_TRUNK_DIR]"
+
+mkdir -p output || die "Can't create output dir."
+
+rm -rf build/gcs-gen 2>/dev/null
+
+mkdir -p build/gcs-gen || die "Can't create build dir."
+cd build/gcs-gen || die "Can't enter build dir."
+
+echo -n "copying trunk... "
+
+cp -r $_TRUNK_DIR trunk/ || die "failed to copy"
+cd trunk || die "failed to enter dir"
+
+echo "done"
+echo -n "cleaning... "
+
+./clean.sh
+
+echo "done"
+
+CL_DATE="$(date '+%a, %d %b %Y %H:%M:%S %z')"
+
+echo "updating changelog"
+
+mv ChangeLog ChangeLog.orig
+
+head -n 1 ChangeLog.orig | sed -e "s/^$ORIG_NAME /$PKG_NAME /" > ChangeLog
+echo -e -n "\n  * Repackaging for $PKG_NAME\n\n" >> ChangeLog
+echo -e -n " -- Tarick Bedeir <tarick@bedeir.com>  $CL_DATE\n\n" >> ChangeLog 
+
+cat ChangeLog.orig >> ChangeLog
+rm -f ChangeLog.orig
+
+echo -n "renaming... "
+
+for F in \
+  INSTALL \
+  README \
+  $(find . -name \*.am);
+do
+  mv $F $F.orig
+  sed \
+    -e "s/$ORIG_NAME/$PKG_NAME/g" \
+    < $F.orig > $F
+  chmod $(stat -f %p $F.orig) $F
+  rm -f $F.orig
+done
+
+for F in $(find . -name \*$ORIG_NAME\*); do
+  mv $F ${F/$ORIG_NAME/$PKG_NAME} || die "can't rename $F"
+done
+
+echo "done"
+
+echo "configuring..."
+
+./build.sh $* >/dev/null || die "Can't configure."
+
+echo "done"
+
+echo -n "making tarball... "
+
+make dist >/dev/null || die "failed"
+
+echo "done"
+
+mv *.tar.gz $PKG_DIR/output/
+
+cd $PKG_DIR || die "Can't return to packaging dir."
+rm -rf build/gcs-gen
+rmdir build 2>/dev/null
+
+echo "tarball(s) are in [$PKG_DIR/output/]"
